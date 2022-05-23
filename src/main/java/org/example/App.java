7c3670c6fa4fc34e8863270302cbf028da96ea9b
Joao <joaolongp14@gmail.com>
2022-05-23 00:44:47 -0300

    first commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class App{
+    public static void main(String[] args) throws IOException {
+        String fileName = "src\\main\\java\\org\\example\\resources\\test.docx";
+
+        try (XWPFDocument doc = new XWPFDocument()) {
+
+            // create a paragraph
+            XWPFParagraph p1 = doc.createParagraph();
+            p1.setAlignment(ParagraphAlignment.CENTER);
+
+            // set font
+            XWPFRun r1 = p1.createRun();
+            r1.setBold(true);
+            r1.setItalic(true);
+            r1.setFontSize(22);
+            r1.setFontFamily("New Roman");
+            r1.setText("I am first paragraph.");
+
+            // save it to .docx file
+            try (FileOutputStream out = new FileOutputStream(fileName)) {
+                doc.write(out);
+            }
+
+        }
+    }
+}
